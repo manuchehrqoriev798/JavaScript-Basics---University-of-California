24b6e6235f3da2c22d493504165d937f10d62797
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-01-28 18:28:19 +0600

    continuation tomorow morning

--- test/test.js
+++ test/test.js
@@ -17,17 +17,28 @@ const myProfile = {          // creats object
     myInfo: {                // object inside of object
         netWorth: 10^6,
         mentalHealth: 'stable'
-    }                 
+    },                       // don't forget about coma (,) in order to add more property to the object
+    myChildren: function () {
+        console.log('hello')
+    }, 
+    myChildren(){
+        console.log('hello')      // short form of writing a method
+    },      
 }
+console.log(myProfile.myGoal)             // calling property
+console.log(myProfile.myChildren())       // calling method   //bug: why this is showing underfined and not showing just hello
 delete myProfile.myInfo.netWorth
 myProfile.myAdress = 'Naryn' // adding new value
 delete myProfile.myAdress    // deleting value
 console.log(myProfile)       // showed
 const myPassport = myProfile // created new link to the object
 myPassport.myAge = 'Eighteen'// edited the value that was given 
-const myAdress = 'Earth'     // created new style with name Earth 
-myProfile[myAdress]= 'Tjk'   // connected the style name to object and adds value to it 
-delete myProfile[myAdress]   // deletes new created style and value through square breckets
-console.log(myPassport)      // shows the final result
-
+const myAdress = 'Earth'     // created new property with name Earth 
+myProfile[myAdress]= 'Tjk'   // connected the property name to object and adds value to it 
+delete myProfile[myAdress]   // deletes new created preperty through square breckets
+console.log(myProfile.myChildren())
+console.log(myProfile.myGoal)
+console.log(myPassport)      // shows the final result   // window.console.log(myProfile) showed using global object window. window.console and just console the same 
+JSON.stringify(myProfile)    //bug: not showing convertation
+ 
 
