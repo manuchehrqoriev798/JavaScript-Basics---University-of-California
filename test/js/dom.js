08014457ec40757f0e5ffa48e51684d1fa73295d
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2023-02-10 14:46:13 +0600

    first time changing styles through js not css

--- test/js/dom.js
+++ test/js/dom.js
@@ -5,11 +5,18 @@
 
 const header = document.getElementById('header')
 console.log(header)
+console.dir(header.textContent)
+header.textContent = 'Hello DOM !'
+console.dir(header.textContent)
 
-
-
-
-
+header.style.color  = 'red'
+header.style.textAlign  = 'center'
+header.style.backgroundColor = 'gold'
+header.style.padding = '2rem'
+header.style.textShadow = '3px 3px 20px white'
+header.style.textTransform = 'uppercase'
+header.style.wordSpacing = '20px'
+header.style.letterSpacing = '5px'
 
 
 
